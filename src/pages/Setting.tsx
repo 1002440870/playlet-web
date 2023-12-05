2eb000c7a8025dcf2be626e410dc38429768762f
Admin <sslz120@163.com>
2023-12-05 20:57:34 +0800

    first commit

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -1,9 +1,35 @@
 import PageLayout from "@/components/PageLayout";
+import styles from "@/styles/pages/Settings.module.scss";
+import { Image, Switch } from "antd-mobile";
 
 const Setting = () => {
+    const SettingsMenu = [
+        { title: "Disable AutoPlay on 4G/5G" },
+        { title: "Clear cache" },
+        { title: "Rate Us" },
+        { title: "Release Code", exp: "v1.0.0" },
+        { title: "Terms of Service" },
+        { title: "Privacy Policy" },
+    ]
+
     return (
-        <PageLayout title={"Setting"}>
-            <div>设置</div>
+        <PageLayout title={"Settings"}>
+            <div className={styles.container}>
+                {SettingsMenu.map((item, index) => (
+                    <div className={styles.settings_cards} key={index}>
+                        <div className={styles.left}>{item.title}</div>
+                        <div className={styles.right}>
+                            {item.exp && <div className={styles.exp}>{item.exp}</div>}
+                            {index === 0 ? <Switch className={styles.switch_icon} /> :
+                                <Image className={styles.more_icon} src={require("@/assets/more.png")} />}
+                        </div>
+                    </div>
+                ))}
+                <div className={styles.copy_view}>
+                    <div className={styles.text}>Copyright ©2022 PlatApp</div>
+                    <div className={styles.text}>All Rights Reserved</div>
+                </div>
+            </div>
         </PageLayout>
     )
 }
